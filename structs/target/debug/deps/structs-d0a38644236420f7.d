61ead039a1f8efe44a806c88f3e800e512cf257c
a1 <82595427+salugi@users.noreply.github.com>
2022-02-19 08:21:34 -0600

    Delete structs/target directory

--- structs/target/debug/deps/structs-d0a38644236420f7.d
+++
@@ -1,6 +0,0 @@
-/Users/a1/Projects/arti/structs/target/debug/deps/structs-d0a38644236420f7.rmeta: src/lib.rs src/points.rs
-
-/Users/a1/Projects/arti/structs/target/debug/deps/structs-d0a38644236420f7.d: src/lib.rs src/points.rs
-
-src/lib.rs:
-src/points.rs:
--- structs/target/debug/deps/structs-d3b4e5d7fdd432ad.d
+++
@@ -1,6 +0,0 @@
-/Users/a1/Projects/arti/structs/target/debug/deps/structs-d3b4e5d7fdd432ad.rmeta: src/lib.rs src/points.rs
-
-/Users/a1/Projects/arti/structs/target/debug/deps/structs-d3b4e5d7fdd432ad.d: src/lib.rs src/points.rs
-
-src/lib.rs:
-src/points.rs:
